8ae42dd84306de9021fc0e6d668419da83499431
Matthew Shepard <mattshepard@gmail.com>
2015-03-17 11:27:09 -0700

    Restructure, expose test runner

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 'use strict';
 
-module.exports = require('require-dir')('./lib');
+var _ = require('lodash');
+var when = require('when');
+var testRunner = require('./lib/testRunner');
+var prepareRequest = require('./lib/prepareRequest');
+
+function defaultWrapper(request, execute){
+  console.log(request.method+' '+request.url);
+  return execute();
+}
+
+function startTests(server, requests, testWrapper){
+  if(!_.isFunction(testWrapper)){
+    testWrapper = defaultWrapper;
+  }
+  if(_.isArray(requests)){
+    _.forEach(requests, function(request){
+      testRunner(server, prepareRequest(request), when.resolve(request.args), testWrapper);
+    });
+  }else if(_.isObject(requests)){
+    testRunner(server, prepareRequest(requests), when.resolve(requests.args), testWrapper);
+  }
+}
+
+module.exports = startTests;
